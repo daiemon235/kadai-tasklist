ed9613435d4b39627de47fbd5ac0c9c24bb95951
daisuke.iwasaki <daiemon.odakyu4000@gmail.com>
2024-04-01 22:38:19 +0900

    NewServlet.javaを作成

--- src/controllers/NewServlet.java
+++ src/controllers/NewServlet.java
@@ -1,15 +1,17 @@
 package controllers;
 
 import java.io.IOException;
+import java.sql.Timestamp;
 
-import javax.servlet.RequestDispatcher;
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import models.Message;
+import models.Task;
+import utils.DBUtil;
 
 /**
  * Servlet implementation class NewServlet
@@ -29,13 +31,28 @@ public class NewServlet extends HttpServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // CSRF対策
-        request.setAttribute("_token", request.getSession().getId());
+        EntityManager em = DBUtil.createEntityManager();
+        em.getTransaction().begin();
 
-        // おまじないとしてのインスタンスを生成
-        request.setAttribute("message", new Message());
+        // Taskのインスタンスを生成
+        Task t = new Task();
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
-        rd.forward(request, response);
+        // mの各フィールドにデータを代入
+
+        String content = "test";
+        t.setContent(content);
+
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());     // 現在の日時を取得
+        t.setCreated_at(currentTime);
+        t.setUpdated_at(currentTime);
+
+        // データベースに保存
+        em.persist(t);
+        em.getTransaction().commit();
+
+        // 自動採番されたIDの値を表示
+        response.getWriter().append(Integer.valueOf(t.getId()).toString());
+
+        em.close();
      }
 }
